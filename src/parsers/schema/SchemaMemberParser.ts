7000cd931c8c76f76261a61dcafaa01b81a46bc9
b01010110 <b01010110b@gmail.com>
2022-09-11 06:07:05 +0700

    SchemaMemberParser has been added

---
+++ src/parsers/schema/SchemaMemberParser.ts
@@ -0,0 +1,9 @@
+/**
+ * Schema member parser is abstract class for inheritance by parsers.
+ */
+export default abstract class SchemaMemberParser {
+  /**
+   * Identifier.
+   */
+  public static identifier: string
+}
