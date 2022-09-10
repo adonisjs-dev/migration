1ec316dafbc054bd918d922a2ec3e791ab112269
b01010110 <b01010110b@gmail.com>
2022-09-11 04:40:01 +0700

    MigrationFile has been added

---
+++ src/MigrationFile.ts
@@ -0,0 +1,53 @@
+import { SourceFile } from 'ts-morph'
+import MigrationParser from './parsers/MigrationParser'
+import MigrationAction from './actions/MigrationAction'
+
+/**
+ * Migration file is the migration file object that located in the file system.
+ * It performs actions relative to the current migration file.
+ */
+export default class MigrationFile {
+  /**
+   * Table name.
+   */
+  public tableName = ''
+
+  /**
+   * Method Up.
+   */
+  public up: MigrationAction[] = []
+
+  /**
+   * Method Down.
+   */
+  public down: MigrationAction[] = []
+
+  /**
+   * Source file.
+   */
+  public sourceFile: SourceFile
+
+  /**
+   * The constructor.
+   */
+  public constructor(sourceFile: SourceFile) {
+    this.sourceFile = sourceFile
+    this.parse()
+  }
+
+  /**
+   * Parse the file.
+   */
+  public parse() {
+    this.tableName = MigrationParser.extractTableName(this.sourceFile)
+    this.up = MigrationParser.parseUpMethod(this.sourceFile)
+    this.down = MigrationParser.parseDownMethod(this.sourceFile)
+  }
+
+  /**
+   * Get the file path.
+   */
+  public get filePath() {
+    return this.sourceFile.getFilePath()
+  }
+}
