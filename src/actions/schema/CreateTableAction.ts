f3865f6266a891a90ea08567ecce8d874d557a06
b01010110 <b01010110b@gmail.com>
2022-09-11 14:02:57 +0700

    CreateTableAction has been added

---
+++ src/actions/schema/CreateTableAction.ts
@@ -0,0 +1,21 @@
+import MigrationAction from '../MigrationAction'
+
+/**
+ * Create table action is an object containing information to create a table.
+ */
+export default class CreateTableAction extends MigrationAction {
+  /**
+   * Table name.
+   */
+  public name = ''
+
+  /**
+   * Schema name.
+   */
+  public schema = ''
+
+  /**
+   * Table column actions.
+   */
+  public columns: string[] = []
+}
