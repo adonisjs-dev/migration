a129789ca5a4da89cb486d7d80f28417d1b346d9
b01010110 <b01010110b@gmail.com>
2022-09-11 14:03:22 +0700

    DropTableAction has been added

---
+++ src/actions/schema/DropTableAction.ts
@@ -0,0 +1,20 @@
+import MigrationAction from '../MigrationAction'
+/**
+ * Drop table action is an object containing information to drop a table.
+ */
+export default class DropTableAction extends MigrationAction {
+  /**
+   * Table name.
+   */
+  public name = ''
+
+  /**
+   * Schema name.
+   */
+  public schema = ''
+
+  /**
+   * Table column actions.
+   */
+  public columns: string[] = []
+}
