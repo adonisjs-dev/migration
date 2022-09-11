ec6903e4858eea35fc79fc21ec3ff3b819048d07
b01010110 <b01010110b@gmail.com>
2022-09-11 13:31:10 +0700

    CreatableActionParser has been added

---
+++ src/parsers/schema/CreatableActionParser.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { CallExpression } from 'ts-morph'
+import SchemaMemberParser from './SchemaMemberParser'
+import MigrationAction from '../../actions/MigrationAction'
+
+/**
+ * Creatable action parser is abstract class for inheritance by parsers.
+ */
+export default abstract class CreatableActionParser extends SchemaMemberParser {
+  /**
+   * Parse a Call Expression Node.
+   */
+  public static parse(ceNode: CallExpression): MigrationAction | undefined {
+    return
+  }
+}
