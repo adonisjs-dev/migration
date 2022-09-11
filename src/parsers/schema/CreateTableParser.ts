3c8127e8e9db3b717c0ad0e5a0e8dd9d0f3e2a03
b01010110 <b01010110b@gmail.com>
2022-09-11 13:33:39 +0700

    CreateTableParser has been added

---
+++ src/parsers/schema/CreateTableParser.ts
@@ -0,0 +1,21 @@
+import { CallExpression } from 'ts-morph'
+import CreatableActionParser from './CreatableActionParser'
+import MigrationAction from '../../actions/MigrationAction'
+import CreateTableAction from '../../actions/schema/CreateTableAction'
+
+/**
+ * Create Table Parser parses the createTable method.
+ */
+export default abstract class CreateTableParser extends CreatableActionParser {
+  /**
+   * Identifier.
+   */
+  public static identifier = 'createTable'
+
+  /**
+   * Parse a Call Expression Node.
+   */
+  public static parse(ceNode: CallExpression): MigrationAction | undefined {
+    return new CreateTableAction()
+  }
+}
