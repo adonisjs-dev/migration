221f04800c96b04a3819bee2fc0b18813c1f895c
b01010110 <b01010110b@gmail.com>
2022-09-11 13:40:37 +0700

    WithSchemaParser has been added

---
+++ src/parsers/schema/WithSchemaParser.ts
@@ -0,0 +1,27 @@
+import { SyntaxKind, CallExpression } from 'ts-morph'
+import PropertyParser from './PropertyParser'
+
+/**
+ * With Schema Parser parses the withSchema method.
+ */
+export default abstract class WithSchemaParser extends PropertyParser {
+  /**
+   * Identifier.
+   */
+  public static identifier = 'withSchema'
+
+  /**
+   * Parse a Call Expression Node.
+   */
+  public static parse(ceNode: CallExpression): { [name: string]: string } {
+    const properties: { [name: string]: string } = {}
+
+    const args = ceNode.getArguments()
+    const arg1 = args[0].asKind(SyntaxKind.StringLiteral)
+    if (!arg1) return properties
+
+    properties.schema = arg1.getLiteralValue()
+
+    return properties
+  }
+}
