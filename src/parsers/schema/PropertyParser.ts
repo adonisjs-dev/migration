d14e040aacfe06db58875b60117549f5a2d6f47b
b01010110 <b01010110b@gmail.com>
2022-09-11 13:31:42 +0700

    PropertyParser has been added

---
+++ src/parsers/schema/PropertyParser.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { CallExpression } from 'ts-morph'
+import SchemaMemberParser from './SchemaMemberParser'
+
+/**
+ * Property parser is abstract class for inheritance by parsers.
+ */
+export default abstract class PropertyParser extends SchemaMemberParser {
+  /**
+   * Parse a Call Expression Node.
+   */
+  public static parse(ceNode: CallExpression): { [name: string]: string } {
+    return {}
+  }
+}
