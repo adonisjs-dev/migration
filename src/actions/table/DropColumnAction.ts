11212da6ffffae6879314da7e1cda0703e046f8f
b01010110 <b01010110b@gmail.com>
2022-12-01 09:17:58 +0700

    DropColumnAction has been added

---
+++ src/actions/table/DropColumnAction.ts
@@ -0,0 +1,11 @@
+import TableAction from '../TableAction'
+
+/**
+ * Drop column action is an object containing information to drop a column.
+ */
+export default class DropColumnAction extends TableAction {
+  /**
+   * Column name.
+   */
+  public name = ''
+}
