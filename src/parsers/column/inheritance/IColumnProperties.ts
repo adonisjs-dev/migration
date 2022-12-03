0f78b7d3d51e98e4b3826470bce7bc561a6996e0
b01010110 <b01010110b@gmail.com>
2022-12-03 12:31:38 +0700

    new properties of IColumnProperties have been added

--- src/parsers/column/inheritance/IColumnProperties.ts
+++ src/parsers/column/inheritance/IColumnProperties.ts
@@ -1,4 +1,11 @@
 export default interface IColumnProperties {
   isAlter?: boolean
   isNullable?: boolean
+  isUnique?: boolean
+  isUnsigned?: boolean
+  default?: string
+  references?: string
+  inTable?: string
+  onUpdate?: string
+  onDelete?: string
 }
